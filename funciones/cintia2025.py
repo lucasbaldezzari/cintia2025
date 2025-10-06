62ace34b0b2c0eabb10871b295eddcd1bf282937
lucasbaldezzari <lmbaldezzari@gmail.com>
2025-10-05 23:43:38 -0300

    Se actualiza repo

--- funciones/cintia2025.py
+++ funciones/cintia2025.py
@@ -1,1 +1,22 @@
-pass
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+def load_housing_data():
+    try:
+        data = pd.read_csv("cintia2025\\datasets\\housing\\housing.csv")
+    except FileNotFoundError:
+        data = pd.read_csv("datasets\\housing\\housing.csv")
+
+    return data
+
+def plot_histograms(data, figsize=(14, 8), bins=50, color="#5f4db1"):
+    """
+    data: Es el dataframe que contiene los datos de housing. Se supone que es el dataframe
+    original, con todas las columnas.
+    """
+    data.hist(figsize=figsize, bins=bins, color=color)
+    plt.show()
+    return None
